a728bf2d282ce56845200ae607da05e69444362b
Mohamad amin Katebsaber <katebsaber@ymail.com>
2019-06-29 19:16:07 +0430

    temperature itteratins added

--- monte carlo/main.py
+++ monte carlo/main.py
@@ -3,44 +3,47 @@ from matplotlib import pyplot as plt
 import numpy as np
 import time
 
-T = 0.5
+# T = 0.5
 box_dimention = 10
 number_of_particles = 30
 
-box = create_box(box_dimention,number_of_particles)
-particles_pos_idx = get_particles_pos(box)
-
-start = time.time()
-list_of_e = []
-for i in range(3000):
-    if (i%100) == 0: print(i) # print itteration number every 100 steps
+for j in range(1,41):
+    T = 0.5 * j
+        
+    box = create_box(box_dimention,number_of_particles)
+    particles_pos_idx = get_particles_pos(box)
     
-    e_temp = 0 # zero out temp energy
-    
-    # do the move
-    status,box_temp,particles_pos_idx_temp = move_one_particle(box,particles_pos_idx)
-    while status != True:
+    start = time.time()
+    list_of_e = []
+    for i in range(3000):
+        if (i%100) == 0: print(i) # print itteration number every 100 steps
+        
+        e_temp = 0 # zero out temp energy
+        
+        # do the move
         status,box_temp,particles_pos_idx_temp = move_one_particle(box,particles_pos_idx)
-    
-    #calculate energies
-    e = calculate_energy(box,particles_pos_idx)
-    e_temp = calculate_energy(box_temp,particles_pos_idx_temp)
+        while status != True:
+            status,box_temp,particles_pos_idx_temp = move_one_particle(box,particles_pos_idx)
+        
+        #calculate energies
+        e = calculate_energy(box,particles_pos_idx)
+        e_temp = calculate_energy(box_temp,particles_pos_idx_temp)
 
-    # check move acceptance
-    if e_temp <= e:
-        box, particles_pos_idx = box_temp,particles_pos_idx_temp
-        list_of_e.append(e_temp)
-    else:
-        p = np.exp(-1 * (e_temp-e) / T) # this shoud change later!
-        accept = np.random.choice(np.arange(0,2), p=[1-p,p])
-
-        if accept:
+        # check move acceptance
+        if e_temp <= e:
             box, particles_pos_idx = box_temp,particles_pos_idx_temp
             list_of_e.append(e_temp)
+        else:
+            p = np.exp(-1 * (e_temp-e) / T) # this shoud change later!
+            accept = np.random.choice(np.arange(0,2), p=[1-p,p])
 
-end = time.time()
-print('elapsed time: ', end - start)
+            if accept:
+                box, particles_pos_idx = box_temp,particles_pos_idx_temp
+                list_of_e.append(e_temp)
 
-# print(len(list_of_e))
-plt.plot(list_of_e)
-plt.show()
+    end = time.time()
+    print('elapsed time: ', end - start)
+
+    # print(len(list_of_e))
+    plt.plot(list_of_e)
+    plt.show()
